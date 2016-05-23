0c19d7144075aefbb7d97e35a5db4313beb3ac4c
domiurg-home <maxdom9@yandex.ru>
2016-05-23 20:59:05 +0800

    Print SQL query data to console

--- src/domiurg/SQLConnect.java
+++ src/domiurg/SQLConnect.java
@@ -1,9 +1,6 @@
 package domiurg;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SQLConnect {
     private Connection connect = null;
@@ -17,8 +14,33 @@ public class SQLConnect {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             connect = DriverManager.getConnection(URL, "arduino_user", "sometext");
 
+            statement = connect.createStatement();
+
+            String query = "SELECT *" +
+                            " FROM `90:a2:da:f8:0d:d4_data_2`" +
+                            " ORDER BY `id` DESC" +
+                            " LIMIT 50";
+            res = statement.executeQuery(query);
+            getResult(res);
+
         } catch (Exception e){
             throw e;
+        }
+    }
+
+    public void getResult(ResultSet res) throws SQLException{
+        while (res.next()){
+            // Print results to console as debug info
+            String id = res.getString("id");
+            String time = res.getString("time");
+            String temp = res.getString("temperature");
+            String humi = res.getString("humidity");
+
+            System.out.println("ID: " + id);
+            System.out.println("Time: " + time);
+            System.out.println("Temperature: " + temp);
+            System.out.println("Humidity: " + humi);
+            System.out.println("");
         }
     }
 
